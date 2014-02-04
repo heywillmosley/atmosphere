fc983e5f556aaa93c1b760c403532e997ac74159
William Mosley <wmosley@kingbio.com>
2014-02-04 09:27:41 -0500

    initial commit

---
+++ js/site-footer.js
@@ -0,0 +1,5 @@
+//@prepros-append atmosphere/atmosphere-footer.js
+//@prepros-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shCore.js
+//@prepros-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shBrushJScript.js
+//@prepros-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shBrushXml.js
+//@prepros-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shBrushCSS.js
