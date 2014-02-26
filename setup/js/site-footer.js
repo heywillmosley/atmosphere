cc36d1366609597726b038977bcc7feffeb7bee4
William Mosley <wmosley@kingbio.com>
2014-02-26 11:58:51 -0500

    added setup

---
+++ setup/js/site-footer.js
@@ -0,0 +1,5 @@
+/* Vendor */
+
+/* Components */
+//@prepros-append ../atmosphere/js/journey.js
+//@prepros-append ../atmosphere/js/exhibit.js
---
+++ setup/js/site-header.js
@@ -0,0 +1,13 @@
+// Modernizr
+//@prepros-append ../atmosphere/vendor/modernizr-2.71.custom/custom.modernizr.min.js
+
+// Windows Phone Resolution Fix
+(function() {
+    if ("-ms-user-select" in document.documentElement.style && navigator.userAgent.match(/IEMobile\/10\.0/)) {
+        var msViewportStyle = document.createElement("style");
+        msViewportStyle.appendChild(
+            document.createTextNode("@-ms-viewport{width:auto!important}")
+        );
+        document.getElementsByTagName("head")[0].appendChild(msViewportStyle);
+    }
+})();
