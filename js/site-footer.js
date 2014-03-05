a706d0ab313af8634b18e32d2848f53932c398ed
William Mosley <will@livesuperamazing.com>
2014-03-05 07:55:17 -0500

    added more elements

--- js/site-footer.js
+++ js/site-footer.js
@@ -1,12 +1,26 @@
 /* Vendor */
 
 //@prepros-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shCore.js
+//@codekit-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shCore.js
+
 //@prepros-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shBrushJScript.js
+//@codekit-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shBrushJScript.js
+
 //@prepros-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shBrushXml.js
+//@codekit-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shBrushXml.js
+
 //@prepros-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shBrushCSS.js
+//@codekit-append ../vendor/syntaxhighlighter_3.0.83.custom/scripts/shBrushCSS.js
 
 /* Components */
 //@prepros-append journey.js
+//@codekit-append journey.js
+
 //@prepros-append exhibit.js
+//@codekit-append exhibit.js
+
 //@prepros-append dropdown.js
+//@codekit-append dropdown.js
+
 //@prepros-append footstep.js
+//@codekit-append footstep.js
