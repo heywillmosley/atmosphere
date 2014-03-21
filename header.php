2240bae01c3f696a8e67077c71f26d7cdd72465d
William Mosley <wmosley@kingbio.com>
2014-03-21 08:23:59 -0400

    updated atmosphere

---
+++ header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7 ie6" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8 ie7" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9 ie8" lang="en"> <![endif]-->
+<!--[if IE 9]>    <html class="no-js lt-ie10 ie9" lang="en"> <![endif]-->
+<!--[if gt IE 9]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+    <meta charset="utf-8">
+    <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> in HTACCESS FILE to Avoid Validation Error -->
+    <title>Atmosphere</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="keywords" content=""/>  
+    <meta name="author" content="William Mosley, III <wmosley@kingbio.com>">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php root_url(); ?>img/favicons/favicon-114.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php root_url(); ?>img/favicons/favicon-72.png">
+    <link rel="shortcut icon" href="<?php root_url(); ?>img/favicons/favicon-16.png">
+    <!--[if IE]><link rel="shortcut icon" href="img/favicons/favicon-16.icon"><![endif]-->
+    <!-- or, set /favicon-16.ico for IE10 win -->
+    
+    <link rel="stylesheet" type="text/css" href="<?php root_url(); ?>dist/css/atmosphere-site.min.css">
+    <script language="javascript" type="text/javascript" src="<?php root_url(); ?>dist/js/site-header.min.js"></script>
+</head>        
+
+<body>
+	<header class="max-width-960">
+  	<ul class="main-nav">
+    	<li class="main-nav-logo"><a href="#">Company Logo</a></li>
+      <li><a href="#">New</a></li>
+      <li><a href="#">Funiture</a></li>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Bed &amp; Bath</a></li>
+    </ul><!-- end main-nav -->
+  </header>
